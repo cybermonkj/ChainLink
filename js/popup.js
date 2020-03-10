406e5429a63e80ab062d44cc37aada0afa5172e3
Joshua Anop <cybermonk@localhost.localdomain>
2020-03-10 16:17:34 +0100

    changed Home

--- js/popup.js
+++ js/popup.js
@@ -9,3 +9,32 @@ $(window).load(function () {
         $('.hover_bkgr_fricc').hide();
     });
 });
+//Darkmode
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+
+  function time(){
+    var today = new Date();
+    var h = today.getHours()
+  if (h>12) {h= h- "12"} ;
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clocky').innerHTML = h + ":" + m + ":" + s;
+  var r = parseInt(s) * 1;
+  var g = parseInt(s) * 3;
+  var b = parseInt(s) * 5;
+  document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+  var t = setTimeout(time, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
+
+time();
